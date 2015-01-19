3c84da5de2286caeff14a9b50bb25c9cafb600c9
Matt Farmer <matt@frmr.me>
2015-01-18 22:46:24 -0500

    txt2md now parses in page metadata.

--- txt2md.sh
+++ txt2md.sh
@@ -15,16 +15,17 @@ for textOrder in $textOrders; do
   base=$(basename $textOrder .txt)
   printf "" > orders/$year/$base.md
 
-  printf "%s\n%s\n%s\n%s\n%s\n%s\n\n%s\n%s\n" \
+  printf "%s\n%s\n%s\n%s\n%s\n" \
     "---" \
-    "title: \"$base\"" \
     "originalname: \"$base\"" \
     "category: orders" \
     "year: $year" \
     "layout: order" \
-    "---" \
-    "<pre>" \
     >> orders/$year/$base.md
+
+  cat metadata/$year/$base >> orders/$year/$base.md
+  echo "---" >> orders/$year/$base.md
+  echo "<pre>" >> orders/$year/$base.md
 
   cat txt/$year/$textOrder >> orders/$year/$base.md
 
